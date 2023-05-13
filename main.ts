af517f918da18428a97dcb78a6e4ab6b214f5b95
pomdtr <achille@deta.sh>
2023-05-13 16:58:19 +0200

    draft sunbeam workflowy extension

---
+++ main.ts
@@ -0,0 +1,39 @@
+import { WorkFlowy } from "https://deno.land/x/workflowy/mod.ts";
+import * as sunbeam from "https://deno.land/x/sunbeam/index.d.ts";
+import TurndownService from "npm:turndown";
+
+const login = Deno.env.get("WORKFLOWY_LOGIN");
+const password = Deno.env.get("WORKFLOWY_PASSWORD");
+if (!login || !password) {
+  console.error(
+    "Please set WORKFLOWY_LOGIN and WORKFLOWY_PASSWORD environment variables"
+  );
+  Deno.exit(1);
+}
+
+const workflowy = new WorkFlowy(login, password);
+const turndownService = new TurndownService();
+
+const document = await workflowy.getDocument();
+
+const items = document.root.items.map(
+  (item) =>
+    ({
+      title: turndownService.turndown(item.name),
+      actions: [
+        {
+          type: "open",
+          title: "Open in Browser",
+          target: `https://workflowy.com/#/${item.id}`,
+        },
+      ],
+    } as sunbeam.Listitem)
+);
+
+const list = {
+  type: "list",
+  title: "Workflowy",
+  items,
+} as sunbeam.List;
+
+console.log(JSON.stringify(list));
