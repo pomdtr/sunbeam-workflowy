d6834b5ba941f5088c204ab896978554d7618b53
pomdtr <achille@deta.sh>
2023-05-13 18:17:43 +0200

    add navigation

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 import { Client } from "https://deno.land/x/workflowy@2.0.0/src/client.ts";
 import { Document } from "https://deno.land/x/workflowy@2.0.0/src/document.ts";
-import * as sunbeam from "https://deno.land/x/sunbeam/index.d.ts";
+import * as sunbeam from "https://deno.land/x/sunbeam@v0.9.26/index.d.ts";
+import { join } from "https://deno.land/std/path/mod.ts";
 import { convert } from "npm:html-to-text";
 
 const dataKey = "workflowy";
+
+const dirname = new URL(".", import.meta.url).pathname;
+const entrypoint = join(dirname, "sunbeam-extension");
 
 const login = Deno.env.get("WORKFLOWY_LOGIN");
 const password = Deno.env.get("WORKFLOWY_PASSWORD");
@@ -13,6 +17,8 @@ if (!login || !password) {
   );
   Deno.exit(1);
 }
+
+const root = Deno.args.length > 0 ? Deno.args[0] : undefined;
 
 const getDocument = async (client: Client) => {
   const wfData = localStorage.getItem(dataKey);
@@ -37,14 +43,20 @@ const getDocument = async (client: Client) => {
 };
 
 const client = new Client(login, password);
-
 const document = await getDocument(client);
 
-const items = document.root.items.map(
+const rootList = root ? document.getList(root) : document.root;
+const items = rootList.items.map(
   (item) =>
     ({
       title: convert(item.name),
       actions: [
+        {
+          type: "push",
+          page: {
+            command: [entrypoint, item.id],
+          },
+        },
         {
           type: "open",
           title: "Open in Browser",
